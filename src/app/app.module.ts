127bfc5e06a08778e3e22d728264aa2b08242aa6
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-10-24 17:24:15 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCardModule, MatCardImage, MatCardContent, MatCardSubtitle,MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatToolbarModule, MatMenuModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ChirpFormComponent } from './chirp-form/chirp-form.component';
+import { ListComponent } from './list/list.component';
+import { SingleComponent } from './single/single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChirpFormComponent,
+    ListComponent,
+    SingleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
